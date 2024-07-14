40d8102f1c9cd9780bde56d118f4ad536e40e4e4
omarharoun <amr0193418@ju.edu.jo>
2024-07-14 14:54:03 +0300

    modified:   chat.py

--- chat.py
+++ chat.py
@@ -4,8 +4,11 @@ import time
 import sqlite3
 import bcrypt
 import os 
+
 os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"]
 os.environ["assistant_id"] == st.secrets["assistant_id"]
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+assistant_id = os.getenv("assistant_id")
 
 # Initialize OpenAI Client
 client = OpenAI(api_key=OPENAI_API_KEY)
